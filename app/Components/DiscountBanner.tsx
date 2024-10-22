1339b6889e502ad095945c0534e930b6450acbb9
HishamAbualhaj <hishamecommerce@gmail.com>
2024-10-22 20:18:17 +0300

    Removing useEffect No need for it now

--- app/Components/DiscountBanner.tsx
+++ app/Components/DiscountBanner.tsx
@@ -3,36 +3,10 @@ import React, { useEffect, useState } from 'react';
 import styles from "./DiscountBanner.module.css";
 
 interface Props {
-  startTime: Date; 
-  endTime: Date;
   announcementText: string;   
 }
 
-const DiscountBanner: React.FC<Props> = ({ startTime, endTime, announcementText }) => {
-  const [isVisible, setIsVisible] = useState<boolean>(false);
-
-  useEffect(() => {
-    const checkVisibility = () => {
-      const now = new Date();
-    
-      if (now >= startTime && now <= endTime) {
-        setIsVisible(true);
-      } else {
-        setIsVisible(false);
-      }
-    };
-
-    checkVisibility(); 
-
-    const interval = setInterval(checkVisibility, 60000); 
-
-    return () => clearInterval(interval);
-  }, [startTime, endTime]);
-
-  if (!isVisible) {
-    return null;
-  }
-
+const DiscountBanner: React.FC<Props> = ({announcementText }) => {
   return (
     <div className={styles.banner}>
       <div className={styles.bannerContent}>
--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,8 @@ import DiscountBanner from "./Components/DiscountBanner";
 
 export default function Home() {
   return (
- <>
-      <DiscountBanner
-        startTime={new Date("2024-10-01T08:00:00")} 
-        endTime={new Date("2024-10-31T23:59:59")} 
-        announcementText="خصومات بنسبة 20% على الكورسات" 
-      />
- </>
+    <>
+      <DiscountBanner announcementText="خصومات بنسبة 20% على الكورسات" />
+    </>
   );
 }
