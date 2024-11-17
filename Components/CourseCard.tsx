c691bb9c7c5a5280ef0aab152e8c845cc6943901
OsamaDW <s12028609@stu.najah.edu>
2024-11-17 15:18:39 +0200

    Add responsive to the course card and change the button prop

--- Components/CourseCard.tsx
+++ Components/CourseCard.tsx
@@ -1,10 +1,10 @@
-import { Box, Flex, Text, Image } from "@chakra-ui/react";
+import { Box, Flex, Text, Image, useBreakpointValue } from "@chakra-ui/react";
 import { colors } from "@/styles/global-info";
 import { Promo } from "./Promo";
 import CustomButton from "./CustomButton";
 import Cart from "@/public/icons/svgCart.svg";
 import ReadMore from "@/public/icons/readMore.svg";
-import Soon from "./soon.svg"
+import Soon from "./soon.svg";
 interface PromoProps {
   courseName: string;
   price: number;
@@ -13,7 +13,7 @@ interface PromoProps {
   numberOfHours: number;
   numberOfMinutes: number;
   icon: string;
-  soon ?: boolean
+  soon?: boolean;
 }
 const CourseCard: React.FC<PromoProps> = ({
   courseName,
@@ -23,71 +23,106 @@ const CourseCard: React.FC<PromoProps> = ({
   numberOfHours,
   numberOfMinutes,
   icon,
-  soon = false
+  soon = false,
 }) => {
-
-  console.log(icon);
+  const numCards = useBreakpointValue({
+    base: <Soon width="110px" height="108px" />,
+    xl: <Soon width="158px" height="155px" />,
+  });
   return (
-    <Promo width={{base: "260px", xl: "372px", '2xl': "350px"}} height={{base: "378px", xl: "526px", '2xl':"509px"}} fontFamily={"Tajawal"} position={'relative'}>
-      {soon && <Box position="absolute" top='-5px' left='-5px' >
-        <Soon width="157.5px" height="154.78px" />
-      </Box>}
+    <Promo
+      width={{ base: "260px", xl: "372px", "2xl": "350px" }}
+      height={{ base: "378px", xl: "526px", "2xl": "509px" }}
+      fontFamily={"Tajawal"}
+      position={"relative"}
+    >
+      {soon && (
+        <Box position="absolute" top="-5px" left="-5px">
+          {numCards}
+        </Box>
+      )}
       <Flex
         width="100%"
-        height="56.2%"
+        height={{ base: "232px", xl: "280px", "2xl": "286px" }}
         bg={colors.color4}
         borderTopRadius={"inherit"}
         justifyContent={"center"}
         alignItems={"center"}
       >
-        <Image src={icon} alt="image" width={"200px"} height={"200px"} />
+        <Image
+          src={icon}
+          alt="image"
+          width={{ base: "126px", xl: "280px", "2xl": "200px" }}
+          height={{ base: "161px", xl: "280px", "2xl": "200px" }}
+        />
       </Flex>
 
       <Flex
         flexDirection={"column"}
         justifyContent={"space-between"}
         width="100%"
-        height="43.8%"
+        height={{ base: "150px", xl: "245px", "2xl": "223px" }}
         bg={"white"}
-        pt={"calc(60px * 0.58)"}
-        pb={"calc(60px - (60px * 0.6667))"}
-        px={"28.25px"}
+        pt={{ base: "15px", xl: "37px", "2xl": "41px" }}
+        pb={{ base: "9px", xl: "28px", "2xl": "26px" }}
+        pl={{ base: "24px", xl: "17px", "2xl": "28.25px" }}
+        pr={{ base: "28px", xl: "24px", "2xl": "28.25px" }}
         textAlign={"right"}
         color={colors.color1}
         borderBottomRadius={"inherit"}
       >
-        <Flex flexDirection={{base: "column"}} >
+        <Flex flexDirection={{ base: "column" }}>
           <Flex
             justifyContent={"space-between"}
-            mt={"-4px"}
-            mb={"-4px"}
             css={{
               "& span": {
                 color: colors.color1,
                 fontWeight: 700,
                 bottom: "10px",
-                
               },
             }}
           >
-            <Text  as="span" fontSize={{base:"16px", xl: "25px", "2xl": "23px"}}>{courseName}</Text>
-            <Text as="span" fontSize={{base:"16px", xl: "25px", "2xl": "23px"}}>${price}</Text>
+            <Text
+              as="span"
+              fontSize={{ base: "16px", xl: "25px", "2xl": "23px" }}
+              pr={{ base: "56px", xl: 0 }}
+            >
+              {courseName}
+            </Text>
+            <Text
+              as="span"
+              fontSize={{ base: "16px", xl: "25px", "2xl": "23px" }}
+            >
+              ${price}
+            </Text>
           </Flex>
-          <Box pt={"10px"} textAlign={{base: "center", xl:"right"}}>{trainer}</Box>
-          <Box fontSize={{xl: "20px", "2xl": "18px"}}>
+          <Box
+            pt={{ base: "7px", xl: "13px", "2xl": "10px" }}
+            pb={{ base: "5px", xl: "13px", "2xl": "4px" }}
+            textAlign={{ base: "center", xl: "right" }}
+          >
+            {trainer}
+          </Box>
+          <Box
+            fontSize={{ xl: "20px", "2xl": "18px" }}
+            pb={{ base: "8px", xl: "17px", "2xl": "15px" }}
+            pr={{ base: "15px", xl: 0 }}
+          >
             {numberOfVedios}فيديو.
             {numberOfHours}ساعة و{numberOfMinutes}دقيقه
           </Box>
         </Flex>
-        <Flex mt={"23px"} justifyContent={"space-between"}>
+        <Flex justifyContent={"space-between"}>
           <CustomButton
             text="أقرا المزيد"
             ButtonColor="green"
+            sizeType="thirdly"
             icon={<ReadMore width="25px" height="25px" />}
           ></CustomButton>
           <CustomButton
-            text="أقرا المزيد"
+            text="شراء"
             ButtonColor="orange"
+            sizeType="thirdly"
             icon={<Cart width="25px" height="25px" />}
           ></CustomButton>
         </Flex>
--- Components/CustomButton.tsx
+++ Components/CustomButton.tsx
@@ -6,19 +6,71 @@ interface CustomButtonProps extends ButtonProps {
   text: string;
   icon?: ReactNode;
   ButtonColor: "green" | "orange";
+  sizeType: "primary" | "secondary" | "thirdly";
 }
 
 const CustomButton: React.FC<CustomButtonProps> = ({
   text,
   icon: icon,
   ButtonColor,
+  sizeType,
   ...rest
 }) => {
+  const width = {
+    widthDesktop:
+      sizeType == "primary"
+        ? "310px"
+        : sizeType == "secondary"
+        ? "200px"
+        : "140px",
+    widthTablet:
+      sizeType == "primary"
+        ? "330px"
+        : sizeType == "secondary"
+        ? "234px"
+        : "160px",
+    widthMobile:
+      sizeType == "primary"
+        ? "200px"
+        : sizeType == "secondary"
+        ? "150px"
+        : "100px",
+  };
+
+  const height = {
+    heightDesktop:
+      sizeType == "primary"
+        ? "80px"
+        : sizeType == "secondary"
+        ? "60px"
+        : "44px",
+    heightTablet:
+      sizeType == "primary"
+        ? "85px"
+        : sizeType == "secondary"
+        ? "70px"
+        : "50px",
+    heightMobile:
+      sizeType == "primary"
+        ? "60px"
+        : sizeType == "secondary"
+        ? "50px"
+        : "40px",
+  };
+
   const { backgroundColor } = background[ButtonColor];
   return (
     <Button
-     
-      size={{sm:"xs", md:"xs", "2xl":"lg"}}
+      width={{
+        base: width.widthMobile,
+        xl: width.widthTablet,
+        "2xl": width.widthDesktop,
+      }}
+      height={{
+        base: height.heightMobile,
+        xl: height.heightTablet,
+        "2xl": height.heightDesktop,
+      }}
       backgroundColor={backgroundColor}
       color="white"
       borderRadius="5px"
