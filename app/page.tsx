b9fbf59c1d450614ca1e93b71afc5b99c6c84207
OsamaDW <s12028609@stu.najah.edu>
2024-11-18 19:25:26 +0200

    remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import React from "react";
 import "@/styles/globals.css";
 import "../styles/swiper_home.css";
 import CourseSection from "@/Components/CourseSection";
-import ResourcesSection from "@/Components/ResourcesSection";
-import ReviewSection from "@/Components/ReviewSection/ReviewSection";
 export default function Home() {
   return (
     <>
