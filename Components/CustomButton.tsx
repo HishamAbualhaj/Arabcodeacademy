cb45a9ffc41f4b46cd55a3f069ff5ce950cae627
HishamAbualhaj <hishamecommerce@gmail.com>
2024-12-05 20:50:32 +0200

    Use another approach to handle button size

--- Components/CustomButton.tsx
+++ Components/CustomButton.tsx
@@ -16,36 +16,53 @@ const CustomButton: React.FC<CustomButtonProps> = ({
   sizeType,
   ...rest
 }) => {
-  const width = {
-    widthDesktop:
-      sizeType == "primary"
-        ? "30px"
-        : sizeType == "secondary"
-        ? "30px"
-        : "25px",
-    widthTablet:
-      sizeType == "primary"
-        ? "30px"
-        : sizeType == "secondary"
-        ? "25px"
-        : "25px",
-    widthMobile:
-      sizeType == "primary"
-        ? "20px"
-        : sizeType == "secondary"
-        ? "15px"
-        : "10px",
-  };
-
   const { backgroundColor } = background[ButtonColor];
-  return (
+  return sizeType == "primary" ? (
     <Button
       _hover={{ bg: ButtonColor === "green" ? "#04B08E" : "#FA4E27" }}
       px={{
-        base: width.widthMobile,
-        xl: width.widthTablet,
-        "2xl": width.widthDesktop,
+        base: "20px",
+        xl: "30px",
+        "2xl": "40px",
       }}
+      py={{
+        base: "32px",
+        "2xl": "35px",
+      }}
+      backgroundColor={backgroundColor}
+      color="white"
+      borderRadius="5px"
+      gap="10px"
+      boxShadow="0px 1px 10px 0px rgba(0, 0, 0, 0.1)"
+      {...rest}
+    >
+      {icon}
+      {text}
+    </Button>
+  ) : sizeType == "secondary" ? (
+    <Button
+      _hover={{ bg: ButtonColor === "green" ? "#04B08E" : "#FA4E27" }}
+      px={{
+        base: "10px",
+        "2xl": "20px",
+      }}
+      py={{
+        base: "25px",
+      }}
+      backgroundColor={backgroundColor}
+      color="white"
+      borderRadius="5px"
+      gap="10px"
+      boxShadow="0px 1px 10px 0px rgba(0, 0, 0, 0.1)"
+      {...rest}
+    >
+      {icon}
+      {text}
+    </Button>
+  ) : (
+    <Button
+      _hover={{ bg: ButtonColor === "green" ? "#04B08E" : "#FA4E27" }}
+      px="10px"
       backgroundColor={backgroundColor}
       color="white"
       borderRadius="5px"
