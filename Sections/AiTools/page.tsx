9e5cc95c685b8c9fd26a5a7f652962228130e960
Tasbeeh Naser <142756718+TasbeehNaser99@users.noreply.github.com>
2025-01-06 01:22:16 +0200

    Update page.tsx

--- Sections/AiTools/page.tsx
+++ Sections/AiTools/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 import Page from '@/app/aiTools/page';
 import React from 'react'
+
 export default function AiToolPage() {
-  
+  const SearchParams {
+  search?: string;
+  isFav?: string;
+  page?: string;
+}
   return (
   <>
-  <Page/>
+  <Page searchParams={searchParams}/>
   </>
   )
 }
