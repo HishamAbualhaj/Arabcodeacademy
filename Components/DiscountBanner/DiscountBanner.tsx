b98d29eb05f3ee709e386444011747b833b9371e
HishamAbualhaj <hishamecommerce@gmail.com>
2025-01-18 13:01:01 +0200

    Remove DiscountBanner.module.css and add chakira ui styling

--- Components/DiscountBanner/DiscountBanner.tsx
+++ Components/DiscountBanner/DiscountBanner.tsx
@@ -1,31 +1,41 @@
 import styles from "@/styles/DiscountBanner.module.css";
+import { Box, Flex } from "@chakra-ui/react";
 interface Props {
-  announcementText: string;   
-  startDate:string;
-  endDate:string;
+  announcementText: string;
+  startDate: string;
+  endDate: string;
 }
-const checkIfDateInRange = (startDate:string , endDate:string): boolean => {
-const SDate = new Date(startDate);
-const EDate = new Date(endDate);
-const now = new Date(); 
-    return now >= SDate && now <= EDate;
-}
+const checkIfDateInRange = (startDate: string, endDate: string): boolean => {
+  const SDate = new Date(startDate);
+  const EDate = new Date(endDate);
+  const now = new Date();
+  return now >= SDate && now <= EDate;
+};
 
-const DiscountBanner: React.FC<Props> = ({announcementText,startDate,endDate }) => {
+const DiscountBanner: React.FC<Props> = ({
+  announcementText,
+  startDate,
+  endDate,
+}) => {
   return (
-<>
-    {checkIfDateInRange(startDate , endDate) && (
-        <div className={styles.banner}>
-          <div className={styles.bannerContent}>
-            <span className={styles.announcementText}>{announcementText}</span>
-          </div>
-        
-        </div>
-    )}
+    <>
+      {checkIfDateInRange(startDate, endDate) && (
+        <Box
+          bgColor="#462576"
+          color="white"
+          padding="15px"
+          lineHeight="30px"
+          fontSize="25px"
+        >
+          <Flex justifyContent="center" alignItems="center" width="100%">
+            <Box fontSize={{ base: "12px", lg: "23px", xl: "25px" }}>
+              {announcementText}
+            </Box>
+          </Flex>
+        </Box>
+      )}
     </>
-  )}
-
-  
-
+  );
+};
 
 export default DiscountBanner;
